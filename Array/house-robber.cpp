104b3e14538eedbe01c4b03c01a0dfbe2e846f28
Rishindra1430 <rishindrasarma1624@gmail.com>
2026-07-24 23:43:30 +0530

    Solved: House Robber [Medium] (Array, Dynamic Programming)

---
+++ Array/house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+         int maxEnergy = INT_MIN;
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        dp[0] = nums[0];
+        
+        for(int i =1;i<n;i++){
+            int pick = nums[i];
+            if(i>1){
+                pick+=dp[i-2];
+            }
+            int nnPick = dp[i-1];
+            dp[i] = max(pick,nnPick);
+        }
+        return dp[n-1];
+    }
+};

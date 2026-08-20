d14cbc73317fda7974f046f5da3e563926b8d1a7
Rishindra1430 <rishindrasarma1624@gmail.com>
2026-08-20 10:15:39 +0530

    Solved: Valid Anagram [Easy] (Hash Table, String, Sorting)

--- Hash-Table/valid-anagram.cpp
+++ Hash-Table/valid-anagram.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.size() != t.size()){
+            return false;
+        }
         unordered_map<char,int> mpp;
         bool isTrue = true;
         for(int i = 0;i<s.size();i++){

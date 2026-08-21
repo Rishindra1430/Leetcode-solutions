60fefb0a70f46cb48dfac8e839e1e06a91f1e367
Rishindra1430 <rishindrasarma1624@gmail.com>
2026-08-21 10:40:22 +0530

    Solved: Number of Substrings Containing All Three Characters [Medium] (Hash Table, String, Sliding Window)

--- Hash-Table/number-of-substrings-containing-all-three-characters.cpp
+++ Hash-Table/number-of-substrings-containing-all-three-characters.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<int> hash = {-1,-1,-1};
-        int cnt = 0;
-        for(int i = 0;i<s.size();i++){
-            hash[s[i] - 'a']  = i;
-            if(hash[0] != -1 && hash[1] != -1 && hash[2] != -1 ){
-                cnt  = cnt + 1 + min({hash[0],hash[1],hash[2]});
-            }
+       vector<int> lastSeen(3,-1);
+       int cnt = 0;
+       for(int i = 0;i<s.size();i++){
+        lastSeen[s[i] - 'a'] = i;
+        if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+            cnt += 1 + min({lastSeen[0],lastSeen[1],lastSeen[2]});
         }
-        return cnt;
+       }
+       return cnt;
     }
 };

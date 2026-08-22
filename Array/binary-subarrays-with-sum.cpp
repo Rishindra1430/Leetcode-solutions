55e3fa1564c813406da0d5e20b9a40203678b93c
Rishindra1430 <rishindrasarma1624@gmail.com>
2026-08-22 13:11:44 +0530

    Solved: Binary Subarrays With Sum [Medium] (Array, Hash Table, Sliding Window, Prefix Sum)

---
+++ Array/binary-subarrays-with-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int goalNum(vector<int> nums, int goal){
+        int l=0 ;
+        int r = 0;
+        int sum =0 ;
+        int cnt = 0;
+        if(goal<0){
+            return 0;
+        }
+        while(r<nums.size()){
+            sum+=nums[r];
+            while(sum > goal){
+                sum = sum - nums[l];
+                l++;
+            }
+            cnt+=r-l+1;
+            r++;
+        }
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+       return goalNum(nums,goal) - goalNum(nums,goal - 1);
+    }
+};
